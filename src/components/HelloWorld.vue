3eacc03707c50ce21add163cb2758821d6860103
W9000586 <loewe0202@163.com>
2020-11-12 12:59:01 +0800

    ref reactive 简单实用

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,59 @@
 <template>
-	<div class="hello">
-		{{ msg }}
-	</div>
+  <div class="hello">
+    {{ msg }}
+    <hr />
+    <div>ref</div>
+    <div>{{ count }}</div>
+    <div>{{ double }}</div>
+    <button @click="increase">click</button>
+    <hr />
+    <div>reactive</div>
+    <div>{{ data.count }}</div>
+    <div>{{ data.double }}</div>
+    <button @click="data.increase">click</button>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-
+import { defineComponent } from 'vue';
+import { ref, computed, reactive } from 'vue';
+interface DataProps {
+  count: number;
+  double: number;
+  increase: () => void;
+}
 export default defineComponent({
-	name: 'HelloWorld',
-	props: {
-		msg: String
-	}
-})
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  setup() {
+    // ref
+    const count = ref(0);
+    const double = computed(() => count.value * 2);
+    const increase = () => count.value++;
+
+    // reactive
+    const data: DataProps = reactive({
+      count: 0,
+      double: computed(() => data.count * 2),
+      increase: () => {
+        data.count++;
+      }
+    });
+    return {
+      count,
+      double,
+      increase,
+      data
+    };
+  }
+});
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-	margin: 40px 0 0;
-}
-ul {
-	list-style-type: none;
-	padding: 0;
-}
-li {
-	display: inline-block;
-	margin: 0 10px;
-}
-a {
-	color: #42b983;
+.hello {
+  color: salmon;
 }
 </style>
